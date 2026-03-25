641a002f89513868529b6bfb8149647e499eb5a7
Sabrina-Chan-Fee <sabrinachanfee@gmail.com>
2026-03-24 21:28:28 -0400

    fix audio and js for buttons is completed

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,52 +1,54 @@
 
-  /*SIMULATE SERVER CODE TO DELETE */
-  const roomCodeContainer = document.getElementById("room-code");
-  const waitRoomBtn = document.getElementById("wait-room-btn");
+/*SIMULATE SERVER CODE TO DELETE */
+const roomCodeContainer = document.getElementById("room-code");
+const waitRoomBtn = document.getElementById("wait-room-btn");
 
-  const roomCode = String(Math.floor(Math.random() * 100000)).padStart(5, "0");
-  roomCodeContainer.textContent = roomCode;
+const roomCode = String(Math.floor(Math.random() * 100000)).padStart(5, "0");
+roomCodeContainer.textContent = roomCode;
 
-  setTimeout(() => {
-    waitRoomBtn.textContent = "Suivant";
-  }, 2000);
+setTimeout(() => {
+  waitRoomBtn.textContent = "Suivant";
+}, 2000);
 
-  //display server nb according to user input
-  let server_nb_input;
-  document.getElementById("submit").onclick = function () {
-    server_nb_input = document.getElementById("server-input").value;
-    console.log(server_nb_input);
-    document.getElementById("server-nb").textContent = server_nb_input;
-  };
+//display server nb according to user input
+let server_nb_input;
+document.getElementById("submit-btn").onclick = function () {
+  server_nb_input = document.getElementById("server-input").value;
+  console.log(server_nb_input);
+  document.getElementById("server-nb").textContent = server_nb_input;
+};
 
-  /* AUDIO */
-  const audio = new Audio("audio/canadian_melody.wav")
-  const playPauseBtn = document.getElementById("play-btn");
-  const audioStatus = document.getElementById("audio-status");
-  const replayBtn = document.getElementById("replay-btn");
+/* AUDIO */
 
-  playPauseBtn.addEventListener("click", () => {
-    if (audio.paused) {
-      audio.play();
-      playPauseBtn.textContent = "Pause"; // change button text
-      playPauseBtn.classList.remove("play");
-      playPauseBtn.classList.add("pause");
-    } else {
-      audio.pause();
-      playPauseBtn.textContent = "Jouez"; // change button text
-      playPauseBtn.classList.remove("pause");
-      playPauseBtn.classList.add("play");
-    }
-  });
+// Melody du canada
+const audio = new Audio("audio/canadian_melody.wav")
+const playPauseBtn = document.getElementById("melody-du-canada-play-btn");
+const audioStatus = document.getElementById("audio-status");
+const replayBtn = document.getElementById("melody-du-canada-replay-btn");
 
-  // Optional: when audio ends, reset button text
-  audio.addEventListener("ended", () => {
-    playPauseBtn.textContent = "Jouez";
-  });
-
-  // Replay on click
-  replayBtn.addEventListener("click", () => {
-    audio.currentTime = 0;
+playPauseBtn.addEventListener("click", () => {
+  if (audio.paused) {
     audio.play();
-  });
+    playPauseBtn.innerHTML = ` <span class="material-symbols-outlined">pause</span>`; //change to pause icon
+    playPauseBtn.classList.remove("play");
+    playPauseBtn.classList.add("pause");
+  } else {
+    audio.pause();
+    playPauseBtn.innerHTML = `<span class="material-symbols-outlined">play_arrow</span>`; // chnage to play icon
+    playPauseBtn.classList.remove("pause");
+    playPauseBtn.classList.add("play");
+  }
+});
+
+// Optional: when audio ends, reset button text
+audio.addEventListener("ended", () => {
+  playPauseBtn.innerHTML = `<span class="material-symbols-outlined">play_arrow</span>`; // chnage to play icon
+});
+
+// Replay on click
+replayBtn.addEventListener("click", () => {
+  audio.currentTime = 0;
+  audio.play();
+});
 
 
