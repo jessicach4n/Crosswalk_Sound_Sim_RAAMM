292a4c6e8fd841bf26521815d7deee5940121b85
jessicach4n <j_chan20@live.concordia.ca>
2026-04-07 12:38:17 -0400

    new sounds added and removed 15 seconds

--- docs/js/sounds.js
+++ docs/js/sounds.js
@@ -2,19 +2,16 @@ import { SoundEngine } from "./sound-engine.js";
 
 export const sounds = {
   // --- Mélodie du Canada (Pass "canadian" so the UI knows which button to animate) ---
-  canadian_melody_15: new SoundEngine("canadian", "audio/canadian_melody_15.wav"),
   canadian_melody_30: new SoundEngine("canadian", "audio/canadian_melody_30.wav"),
   canadian_melody_45: new SoundEngine("canadian", "audio/canadian_melody_45.wav"),
   canadian_melody_60: new SoundEngine("canadian", "audio/canadian_melody_60.wav"),
 
   // --- Bip Bip (Pass "beep") ---
-  beep_15: new SoundEngine("beep", "audio/beep_15.wav"),
   beep_30: new SoundEngine("beep", "audio/beep_30.wav"),
   beep_45: new SoundEngine("beep", "audio/beep_45.wav"),
   beep_60: new SoundEngine("beep", "audio/beep_60.wav"),
 
   // --- Coucou (Pass "cuckoo") ---
-  cuckoo_15: new SoundEngine("cuckoo", "audio/cuckoo_15.wav"),
   cuckoo_30: new SoundEngine("cuckoo", "audio/cuckoo_30.wav"),
   cuckoo_45: new SoundEngine("cuckoo", "audio/cuckoo_45.wav"),
   cuckoo_60: new SoundEngine("cuckoo", "audio/cuckoo_60.wav"),
